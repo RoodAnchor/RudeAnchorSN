73e7e570f1760ab5a2191e5f15305dc54a03cff6
RoodAnchor <ujcn2086@gmail.com>
2024-07-07 13:15:44 +0300

    removed extra stuff

--- RudeAnchorSN/Controllers/HomeController.cs
+++ RudeAnchorSN/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using RudeAnchorSN.Models;
 using System.Diagnostics;
 
 namespace RudeAnchorSN.Controllers
@@ -16,17 +15,6 @@ namespace RudeAnchorSN.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
